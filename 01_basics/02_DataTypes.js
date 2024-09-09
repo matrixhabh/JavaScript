59897c33101bf609ff415d1392b5917d3ef7d62a
Rishabh <rishabh918263@gmail.com>
2024-09-09 23:23:36 +0530

    Data Types Module

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code raedability should be high
+    ;
+
+    let name = "rishabh"
+    let age = 18
+    let isLoggedIN = false
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique components
+
+    console.log(typeof "rishabh"); // string
+    console.log(typeof age);       // number
+    console.log(typeof null);      // object
+    console.log(typeof undefined); // undefined
+
+    
