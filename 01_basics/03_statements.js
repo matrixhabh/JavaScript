b62b0f9bbbbc887d1136fdb1d0d20a1b7eeb899a
Rishabh <rishabh918263@gmail.com>
2024-11-07 23:51:01 +0530

    Statements module

---
+++ 01_basics/03_statements.js
@@ -0,0 +1,18 @@
+// A statement is an instruction to the computer, browser, the JavaScript interpreter to do something.
+
+var first = "wes"; // variable declaration statement
+let age2 = 300;
+const cool1 = true;
+console.log(first); // function call statement
+
+// Code Blocks :-
+
+// ex1:-
+if (age > 10) {
+    console.log("you are old");
+  }
+
+// ex2:-
+if (age > 10) {;
+    console.log("you are old");
+  };
