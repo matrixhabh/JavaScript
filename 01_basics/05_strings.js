7bcf9d67035db0df403661e8680f6ce21ac442ac
Rishabh <rishabh918263@gmail.com>
2024-11-16 22:57:50 +0530

    Strings Module

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "matrixhabh"
+const repoCount = 17
+
+console.log(name + repoCount + " Value");
+
+// Output:
+
+// matrixhabh17 Value 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// to access letter place
+
+const gameName = new String('rishabhhc')
+
+console.log(gameName[0]); // Output:- r
+console.log(gameName.__proto__); // Output:- {}
