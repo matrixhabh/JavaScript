c29dbd4a316b25527cdf9ca44fbdd830b63a5f64
Rishabh <rishabh918263@gmail.com>
2024-11-16 23:08:10 +0530

    Updated Strings Module

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -15,3 +15,18 @@ const gameName = new String('rishabhhc')
 
 console.log(gameName[0]); // Output:- r
 console.log(gameName.__proto__); // Output:- {}
+
+console.log(gameName.length); // Output:- 9
+console.log(gameName.toUpperCase()); // Output:- RISHABHHC
+console.log(gameName.charAt(2)); // Output:- s
+console.log(gameName.indexOf('s')); // Output:- 2
+
+const newString = gameName.substring(0, 4)
+console.log(newString);   // Output:- rish
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString); // Output:- ish
+
+const newStringOne = "   rishabh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
