da6398f02bfd998363851bb704b5ad490c90404b
Rishabh <rishabh918263@gmail.com>
2024-11-24 19:52:37 +0530

    Updated Module

--- 01_basics/06_nums&math.js
+++ 01_basics/06_nums&math.js
@@ -57,4 +57,11 @@ console.log(Math.floor(4.9)); // 4
 console.log(Math.min(4, 3, 6, 8)); // 3
 console.log(Math.max(4, 3, 6, 8)); // 8
 console.log(Math.random()*10 + 1); // generates a random decimal no.
-console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max - min + 1) + min) // Output:- 12.01293300525704
+
+// Output:- Random integers
