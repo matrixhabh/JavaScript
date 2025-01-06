986d947b5bd61a168be7d6e9bdad79065c27f9e3
Rishabh <rishabh918263@gmail.com>
2025-01-06 13:01:33 +0530

    Objects Module

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -25,3 +25,53 @@ console.log((regularUser.fullname));
 
 // To access and store whole object.
 // Output: { userfullname: { firstname: 'Rishabh', lastname: 'Yadav' } }
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign ({}, obj1, obj2, obj4)
+// { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+// { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+// here are a lot of objects under array
+
+users[1].email
+console.log(TinderUser); 
+// { userid: '123abc', name: 'Kratos', isLoggedin: false }
+console.log(Object.keys(TinderUser)); // for printing assigned varibales
+// [ 'userid', 'name', 'isLoggedin' ]
+console.log(Object.values(TinderUser)); // for printing values
+// [ '123abc', 'Kratos', false ]
+console.log(Object.entries(TinderUser));
+// [
+//     [ 'userid', '123abc' ],
+//     [ 'name', 'Kratos' ],
+//     [ 'isLoggedin', false ]
+//   ]
+
+// To save the unentered entries from crashing
+// we use hasOwnProperty function
+
+console.log(TinderUser.hasOwnProperty('isLoggedin'));
+// Gives boolean value : true 
+console.log(TinderUser.hasOwnProperty('isLogged'));
+// Gives boolean value : false 
