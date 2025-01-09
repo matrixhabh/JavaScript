f218564fd42ee47cf050bda93c0d444388b08fde
Rishabh <rishabh918263@gmail.com>
2025-01-09 11:41:29 +0530

    Parameters Module updated

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -53,13 +53,59 @@ function addTwonumbers(number1, number2) {
 }
 
 // Output-
-Rishabh
+// Rishabh
 Result: 9
+
+// Another way to write this-
+function addTwonumbers(number1, number2) {
+    // let result = number1 + number2
+    // console.log("Rishabh");
+    return number1 + number2
+}
 
 const result = addTwonumbers(4, 5)
 
-console.log("Result: ", result);
+// console.log("Result: ", result);
 
 // Output- 
 // 9
 // Result: undefined (here the datatype of the given value is unknown)
+
+// function loginUserMessage(username){
+    // return `${username} just logged in`
+// }
+
+// loginUserMessage("Rishabh") // This is just the reference
+
+// console.log(loginUserMessage("Rishabh"))
+// Rishabh just logged in
+
+// Now if the username isn't given-
+
+// console.log(loginUserMessage());
+// undefined just logged in (not null but undefined)
+
+// Important basic case-
+
+function loginUserMessage(username) {
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} has logged in`
+}
+
+console.log(loginUserMessage());
+
+// If we want to avoid the default if value here 
+// we can just give (username = 'kratos') {i.e., defining a value}
+
+// Output-
+// Please enter a username
+// undefined
+
+// Note these-
+
+// if (undefined) or if ("") are some examples of false values
+// if (!) ( here ! mark turns true to false and false to true)
+// instead of writing (username === undefined) we can just write (!username)
